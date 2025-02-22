b44dc05b8e395dffe82c4574dcd8e078bca35a1b
Lazy-MoMo <abhisekhjoshi74@gmail.com>
2025-02-22 17:39:33 +0545

    added car
    made car work

--- src/trafficsimulator.cpp
+++ src/trafficsimulator.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 std::mutex lightMutex;
 
@@ -31,12 +34,18 @@ public:
 class Road {
 public:
     sf::RectangleShape shape;
+    sf::Text label;
 
-    Road(float x, float y, float width, float height) {
+    Road(float x, float y, float width, float height, const std::string& name, sf::Font& font) {
         shape.setSize(sf::Vector2f(width, height));
         shape.setPosition(x, y);
         shape.setFillColor(sf::Color(50, 50, 50)); // Dark gray road color
-    
+
+        label.setFont(font);
+        label.setString(name);
+        label.setCharacterSize(24);
+        label.setFillColor(sf::Color::White); // Set text color to white
+        label.setPosition(x + width / 2 - label.getGlobalBounds().width / 2, y + height / 2 - label.getGlobalBounds().height / 2);
     }
 };
 
@@ -51,18 +60,50 @@ public:
     }
 };
 
+class Car {
+public:
+    sf::RectangleShape shape;
+    float speedX;
+    float speedY;
+
+    Car(float x, float y, float width, float height, float speedX, float speedY) {
+        shape.setSize(sf::Vector2f(width, height));
+        shape.setPosition(x, y);
+        shape.setFillColor(sf::Color::Blue); // Car color
+        this->speedX = speedX;
+        this->speedY = speedY;
+    }
+
+    void move() {
+        shape.move(speedX, speedY);
+    }
+
+    bool isOutOfBounds(float roadWidth, float roadHeight) {
+        sf::Vector2f pos = shape.getPosition();
+        return (pos.x < 0 || pos.x > roadWidth || pos.y < 0 || pos.y > roadHeight);
+    }
+};
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Traffic Light Simulator",sf::Style::Fullscreen);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Traffic Light Simulator", sf::Style::Fullscreen);
+
+    sf::Font font;
+    if (!font.loadFromFile("/usr/share/fonts/abattis-cantarell-fonts/Cantarell-Light.otf")) {
+        std::cerr << "Error loading font\n";
+        return -1;
+    }
 
     // Traffic lights
-    TrafficLight trafficLight1(450,250,25,100); // Existing traffic light
-    TrafficLight trafficLight2(325,250,25,100); // New traffic light for horizontal road
-    TrafficLight trafficLight3(350,225,100,25); // New traffic light for vertical road
-    TrafficLight trafficLight4(350,350,100,25); // New traffic light for vertical road
+    TrafficLight trafficLight1(450, 250, 25, 100); // Existing traffic light
+    TrafficLight trafficLight2(325, 250, 25, 100); // New traffic light for horizontal road
+    TrafficLight trafficLight3(350, 225, 100, 25); // New traffic light for vertical road
+    TrafficLight trafficLight4(350, 350, 100, 25); // New traffic light for vertical road
 
     // Roads
-    Road road1(200, 250, 400, 100); // Horizontal road x,y,width,height
-    Road road2(350, 100, 100, 400); // Vertical road
+    Road roadA(200, 250, 400, 100, "A", font); // Horizontal road x,y,width,height
+    Road roadB(350, 100, 100, 400, "B", font); // Vertical road
+    Road roadC(200, 250, 400, 100, "C", font); // Horizontal road x,y,width,height
+    Road roadD(350, 100, 100, 400, "D", font); // Vertical road
 
     // Lanes for horizontal road
     Lane lane1(200, 260, 400, 20, sf::Color::White); // First lane
@@ -80,6 +121,10 @@ int main() {
     std::thread lightThread3(&TrafficLight::updateState, &trafficLight3);
     std::thread lightThread4(&TrafficLight::updateState, &trafficLight4);
 
+    // Cars
+    std::vector<Car> cars;
+    std::srand(std::time(nullptr));
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -89,11 +134,45 @@ int main() {
                 window.close();
         }
 
+        // Spawn cars randomly
+        if (std::rand() % 100 < 2) { // 2% chance to spawn a car each frame
+            int side = std::rand() % 4; // Randomly choose one of the four sides
+            if (side == 0) { // Left side
+                float laneY = 260 + (std::rand() % 3) * 30; // Randomly choose one of the horizontal lanes
+                cars.emplace_back(200, laneY, 40, 20, 0.5f, 0.0f); // Add a new car moving horizontally
+            } else if (side == 1) { // Right side
+                float laneY = 260 + (std::rand() % 3) * 30; // Randomly choose one of the horizontal lanes
+                cars.emplace_back(600, laneY, 40, 20, -0.5f, 0.0f); // Add a new car moving horizontally
+            } else if (side == 2) { // Top side
+                float laneX = 360 + (std::rand() % 3) * 30; // Randomly choose one of the vertical lanes
+                cars.emplace_back(laneX, 100, 20, 40, 0.0f, 0.5f); // Add a new car moving vertically
+            } else if (side == 3) { // Bottom side
+                float laneX = 360 + (std::rand() % 3) * 30; // Randomly choose one of the vertical lanes
+                cars.emplace_back(laneX, 500, 20, 40, 0.0f, -0.5f); // Add a new car moving vertically
+            }
+        }
+
+        // Move cars and remove those that are out of bounds
+        for (auto it = cars.begin(); it != cars.end();) {
+            it->move();
+            if (it->isOutOfBounds(800, 600)) {
+                it = cars.erase(it);
+            } else {
+                ++it;
+            }
+        }
+
         {
             std::lock_guard<std::mutex> lock(lightMutex);
             window.clear();
-            window.draw(road1.shape);
-            window.draw(road2.shape);
+            window.draw(roadA.shape);
+            window.draw(roadB.shape);
+            window.draw(roadC.shape);
+            window.draw(roadD.shape);
+            window.draw(roadA.label);
+            window.draw(roadB.label);
+            window.draw(roadC.label);
+            window.draw(roadD.label);
             window.draw(lane1.shape);
             window.draw(lane2.shape);
             window.draw(lane3.shape);
@@ -103,8 +182,12 @@ int main() {
             window.draw(trafficLight1.shape);
             window.draw(trafficLight2.shape);
             window.draw(trafficLight3.shape);
-      window.draw(trafficLight4.shape);
-      
+            window.draw(trafficLight4.shape);
+
+            for (auto& car : cars) {
+                window.draw(car.shape);
+            }
+
             window.display();
         }
     }
@@ -112,5 +195,6 @@ int main() {
     lightThread1.detach();
     lightThread2.detach();
     lightThread3.detach();
+    lightThread4.detach();
     return 0;
 }
